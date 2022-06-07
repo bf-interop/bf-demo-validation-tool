c2844116be2f56c95d9dd7e1476c0445b6f44e74
Jeremy Nelson <jermnelson@gmail.com>
2022-06-07 11:13:17 -0600

    Layout changes and better validation error generation

--- utils.py
+++ utils.py
@@ -6,5 +6,5 @@ from data import SHACL
 def validation_html(validation_result, failure_text: str, graph: rdflib.Graph) -> str:
     validation_result.clear()
     pre = js.document.createElement("pre")
-    pre.innerHTML = failure_text
+    pre.innerHTML = failure_text.replace("<", "&lt;").replace(">", "&gt;")
     validation_result.element.appendChild(pre)
